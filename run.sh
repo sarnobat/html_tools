dca7d0d7ccf363275ffa0241302e2ed6bbc73cc6
Sridhar Sarnobat <ss401533@gmail.com>
2016-11-15 20:10:14 -0800

    Minor changes (untested)

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
+echo '<link rel="stylesheet" type="text/css" href="http://netgear.rohidekar.com/portraits/style.css">' | tee out.html
 cat rohidekar_family_tabs_fixed_cleansed.txt \
 	| perl -pe 's{\t}{*}g'  \
 	| perl -pe 's{(\*+)}{$1 }g' \
 	| perl -pe 's{^}{*}g'  \
 	| groovy list_to_html.groovy 2>/dev/null  \
-	| pbcopy
+	| tee -a out.html
